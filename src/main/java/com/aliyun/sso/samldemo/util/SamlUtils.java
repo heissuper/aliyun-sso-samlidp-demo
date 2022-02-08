aece09baa03c196c9893248c69b04be4811910ba
ningzhong.wyl <ningzhong.wyl@antfin.com>
2022-02-08 17:22:10 +0800

    增加日志

--- src/main/java/com/aliyun/sso/samldemo/util/SamlUtils.java
+++ src/main/java/com/aliyun/sso/samldemo/util/SamlUtils.java
@@ -137,7 +137,7 @@ public class SamlUtils {
 
 	public SamlUtils(){
 	    this(Clock.systemUTC());
-	    System.out.println("abc");
+	    System.out.println("abc王义林");
     }
 
 	public SamlUtils(Clock time) {
